c4a31ad54d423ce3ebbd3fa44198d5dafe5eaf3a
Daniel Nguyen <blindkatana@gmail.com>
2012-11-03 13:53:19 -0700

    Adding comments and find w/ limit

--- src/server/database.py
+++ src/server/database.py
@@ -1,18 +1,54 @@
 import pymongo
 
-# TODO(danny): Replace host/port with production values.
-CONNECTION = pymongo.Connection('localhost', 27017)
-DB = CONNECTION.logs
+class Database(object):
 
-class Database:
+    def __init__(self):
+        # Initialize the connection to the database.
+        # TODO(danny): Replace host/port with production values.
+        connection = pymongo.Connection('localhost', 27017)
+        self.db = connection.logs
+
+    def drop(self, collection):
+        """
+        Drop the given collection from the database.
+
+        Args
+          collection: string - The name of the collection.
+        """
+        self.db[collection].drop()
+
+    def find(self, collection, selector=None, limit=0):
+        """
+        Find all documents in the given collection matching the selector.
+
+        Args
+          collection: string - The name of the collection.
+          selector: dictionary - A dictionary of key-value pairs describing
+              which documents to select from the collection.
+          limit: int - The maximum number of documents to return.
+
+        Return
+          list - A list of documents matching the selector.
+        """
+        cursor = self.db[collection].find(spec=selector, limit=limit)
+        return [item for item in cursor]
+
     def insert(self, collection, document):
+        """
+        Insert a document into the given collection.
+
+        Args
+          collection: string - The name of the collection.
+          document: dictionary - A dictionary of key-value pairs to add into the
+              collection.
+
+        Return
+          boolean - True if the document is successfully inserted.
+        """
         try:
-            DB[collection].insert(document, safe=True)
+            self.db[collection].insert(document, safe=True)
             return True
         except pymongo.errors.OperationFailure:
             return False
-
-    def find(self, collection, selector):
-        return [item for item in DB[collection].find(selector)]
 
 db = Database()
--- tests/server/database_test.py
+++ tests/server/database_test.py
@@ -2,7 +2,6 @@ import sys
 sys.path.insert(0, '../../src/server')
 
 from database import db
-from database import DB
 
 COLLECTION = "test"
 
@@ -12,12 +11,12 @@ def insert_test():
 
     # Check a normal insert.
     if not db.insert(COLLECTION, test_document):
-        print "Normal insert failed"
+        print "insert_test: Normal insert failed"
         return False
 
     # Check a conflicting insert.
     if db.insert(COLLECTION, test_fail_document):
-        print "Conflicting insert failed"
+        print "insert_test: Conflicting insert failed"
         return False
 
     return True
@@ -31,26 +30,50 @@ def find_test():
 
     # Check a normal find.
     if len(db.find(COLLECTION, selector)) != 1:
-        print "Normal selection failed"
+        print "find_test: Normal selection failed"
         return False
 
     # Check an empty find.
     if len(db.find(COLLECTION, fail_selector)) != 0:
-        print "Empty selection failed"
+        print "find_test: Empty selection failed"
+        return False
+
+    return True
+
+def find_stress_test():
+    # Setup test.
+    num_documents = 50
+
+    for i in range(num_documents):
+        db.insert(COLLECTION, {"name": i})
+
+    # Check that all the documents are returned.
+    if len(db.find(COLLECTION)) != num_documents:
+        print "find_stress_test: All document selection failed."
+        return False
+
+    # Check that the correct number of documents are returned.
+    limit = 7
+    if len(db.find(COLLECTION, limit=limit)) != limit:
+        print "find_stress_test: Limited document selection failed."
         return False
 
     return True
 
 def clean():
-    DB[COLLECTION].drop()
+    db.drop(COLLECTION)
 
 def run_tests():
     result = True
+    clean()
 
     result = result and insert_test()
     clean()
 
     result = result and find_test()
+    clean()
+
+    result = result and find_stress_test()
     clean()
 
     if result:
